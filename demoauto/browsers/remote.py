caf7526bf52a6e152bb5f81bc016f6e300c1b210
sonups <sonu.sadasivan@gmail.com>
2021-04-06 00:24:19 +0530

    sdsd

--- demoauto/browsers/remote.py
+++ demoauto/browsers/remote.py
@@ -4,11 +4,17 @@ from demoauto.browsers import WebBrowser
 from demoauto.driver.web_driver import WebDriverOf
 from demoauto.driver.driver import Driver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import os
+
 
 class RemoteBrowser(WebBrowser):
     """Representation of a remote web browser."""
 
     def __init__(self, remote_url: str = "http://localhost:4444/wd/hub") -> None:
+        hub_url = os.getenv('HUB_URL')
+        hub_url = str(hub_url)
+        if hub_url !='None':
+            remote_url = hub_url
         self._remote: WebDriver = Remote(
             command_executor=remote_url, desired_capabilities=DesiredCapabilities.CHROME
         )
