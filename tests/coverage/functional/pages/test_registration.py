61a4f5b6d3f3c0327891b3b9c2e712ebe5ea1e58
sonups <sonu.sadasivan@gmail.com>
2021-04-07 14:47:58 +0530

    xcvcxv

--- tests/coverage/functional/pages/test_registration.py
+++ tests/coverage/functional/pages/test_registration.py
@@ -1,8 +1,9 @@
 from tests.coverage.markers import smoke
 from demoauto.pages.demo import DemoPage
+from demoauto.configuration.base_configuration import BaseConfiguration
 
 @smoke
-def test_request_demo_schedule_now(demo_page: DemoPage) -> None:
+def test_request_demo_schedule_now(base_config: BaseConfiguration, demo_page: DemoPage) -> None:
     assert demo_page.schedule_now().is_displayed()
 
 # @smoke
