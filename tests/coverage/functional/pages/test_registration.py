13c9222fd673720773b23b3c3e4ae6562916dd7d
sonups <sonu.sadasivan@gmail.com>
2021-04-07 10:53:26 +0530

    asdsad

--- tests/coverage/functional/pages/test_registration.py
+++ tests/coverage/functional/pages/test_registration.py
@@ -1,9 +1,9 @@
 from tests.coverage.markers import smoke
 from demoauto.pages.demo import DemoPage
-#
-# @smoke
-# def test_request_demo_schedule_now(demo_page: DemoPage) -> None:
-#     assert demo_page.schedule_now().is_displayed()
+
+@smoke
+def test_request_demo_schedule_now(demo_page: DemoPage) -> None:
+    assert demo_page.schedule_now().is_displayed()
 
 # @smoke
 # def test_header_page_platform(header_page: HeaderPage) -> None:
